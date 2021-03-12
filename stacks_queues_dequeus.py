5a10af94e5086109774254cb83ca14134fc749ea
Arthur Vincent Simon <developarvin@gmail.com>
2021-03-12 11:27:30 +0800

    Partial implementation of is_matched

--- stacks_queues_dequeus.py
+++ stacks_queues_dequeus.py
@@ -15,7 +15,7 @@ class ArrayStack:
     def pop(self):
         if self.is_empty():
             raise Empty('Stack is empty')
-        self.internal_list.pop()
+        return self.internal_list.pop()
 
     def top(self):
         if self.is_empty():
@@ -25,12 +25,37 @@ class ArrayStack:
     def is_empty(self):
         return len(self.internal_list) == 0
 
-AR = ArrayStack()
-AR.push(1)
-AR.pop()
-AR.push(2)
-print(AR.top())
-AR.push(3)
-AR.push(4)
-print(AR.top())
-print(len(AR))
+# AR = ArrayStack()
+# AR.push(1)
+# AR.pop()
+# AR.push(2)
+# print(AR.top())
+# AR.push(3)
+# AR.push(4)
+# print(AR.top())
+# print(len(AR))
+
+# TODO: Not working
+def is_matched(expr):
+    lefty = "[{("
+    righty = "]})"
+    S = ArrayStack()
+
+    for item in expr:
+        if item in lefty:
+            S.push(item)
+        elif item in righty:
+            if S.is_empty():
+                return False
+            try:
+                if lefty.index(item) != righty.index(S.pop()):
+                    return False
+            except ValueError:
+                return False
+
+    return S.is_empty()
+
+
+print(is_matched("[]"))
+print(is_matched("[(5+x)-(y-z)]"))
+print(is_matched("[{()]"))
